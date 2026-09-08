1d2d3ce3c122c7934710ace6a39e1db513ebb123
uri-git23 <uryuuri21@gmail.com>
2026-09-08 13:28:30 +0900

    Fix triangle into hexagon

--- Assets/S03/S03_CustomPolygonMesh.cs
+++ Assets/S03/S03_CustomPolygonMesh.cs
@@ -5,14 +5,26 @@ public class S03_CustomPolygonMesh : MonoBehaviour
 {
     void Start()
     {
+        // 정육각형: 맨 위 꼭짓점에서 시작해 시계방향으로 배치
+        // 각도 = 90도 - i * 60도, 반지름 1
         Vector3[] vertices = new Vector3[]
         {
-            new Vector3( 0f,  1f, 0f), // 0
-            new Vector3( 1f, -1f, 0f), // 1
-            new Vector3(-1f, -1f, 0f), // 2
+            new Vector3( 0f,      1f,     0f), // 0  맨 위
+            new Vector3( 0.8660f, 0.5f,   0f), // 1  오른쪽 위
+            new Vector3( 0.8660f,-0.5f,   0f), // 2  오른쪽 아래
+            new Vector3( 0f,     -1f,     0f), // 3  맨 아래
+            new Vector3(-0.8660f,-0.5f,   0f), // 4  왼쪽 아래
+            new Vector3(-0.8660f, 0.5f,   0f), // 5  왼쪽 위
         };
 
-        int[] triangles = new int[] { 0, 1, 2 };
+        // 부채꼴 분할: 정점 0을 축으로 6-2 = 4개의 삼각형
+        int[] triangles = new int[]
+        {
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4,
+            0, 4, 5,
+        };
 
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
